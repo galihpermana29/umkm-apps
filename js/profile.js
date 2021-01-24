b3f224a25d626277f267c153590780a3f2bee7d7
galihpermana29 <galihcbn123@gmail.com>
2021-01-24 11:33:11 +0700

    pesananku on click, and profile styling img width

--- js/profile.js
+++ js/profile.js
@@ -2,6 +2,14 @@ import { fetching } from '../ajax.js';
 
 function getAllProfileElements() {
 
+   function fetchingPesananPage(){
+      const pesananEl = document.querySelector('.menu-one .pesanan')
+      pesananEl.addEventListener('click', e => {
+         fetching('pesanan')
+      })
+   }
+
+   fetchingPesananPage()
 
 }
 
